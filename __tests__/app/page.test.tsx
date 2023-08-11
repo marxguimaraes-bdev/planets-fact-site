ce77979efcf84bc9b7629ecac5d5828e86acaaed
Marx Guimarães <marx.guimaraes64@gmail.com>
2023-08-11 12:49:23 -0400

    setup test environment

---
+++ __tests__/app/page.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import IndexPage from '@/app/page';
+
+describe('IndexPage', () => {
+  it('works!', () => {
+    expect(true).toBe(true);
+  })
+});
