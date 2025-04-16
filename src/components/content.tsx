22d025b1c32cc247a902bad1f6687eba2a6096af
Marx Guimarães <marx.guimaraes@bairesdev.com>
2025-04-15 21:13:40 -0400

    fix: adjust content layout

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -39,14 +39,14 @@ const buttonClassNames: PlanetT = {
 };
 
 const planetImageSizeClassNames: PlanetT = {
-  Mercury: 'max-h-mercury w-auto md:max-h-md-mercury lg:max-h-lg-mercury my-10',
-  Venus: 'max-h-venus w-auto md:max-h-md-venus lg:max-h-lg-venus',
-  Earth: 'max-h-earth w-auto md:max-h-md-earth lg:max-h-lg-earth',
-  Mars: 'max-h-mars w-auto md:max-h-md-mars lg:max-h-lg-mars',
-  Jupiter: 'max-h-jupiter w-auto md:max-h-md-jupiter lg:max-h-lg-jupiter',
-  Saturn: 'max-h-saturn w-auto md:max-h-md-saturn lg:max-h-lg-saturn',
-  Uranus: 'max-h-uranus w-auto md:max-h-md-uranus lg:max-h-lg-uranus',
-  Neptune: 'max-h-neptune w-auto md:max-h-md-neptune lg:max-h-lg-neptune',
+  Mercury: 'max-h-mercury md:max-h-md-mercury lg:max-h-lg-mercury my-10 lg:ml-[10.625rem]',
+  Venus: 'max-h-venus md:max-h-md-venus lg:max-h-lg-venus lg:ml-[7.1875rem]',
+  Earth: 'max-h-earth md:max-h-md-earth lg:max-h-lg-earth lg:ml-[5.625rem]',
+  Mars: 'max-h-mars md:max-h-md-mars lg:max-h-lg-mars lg:ml-[9.1875rem]',
+  Jupiter: 'max-h-jupiter md:max-h-md-jupiter lg:max-h-lg-jupiter lg:ml-[1.5rem]',
+  Saturn: 'max-h-saturn md:max-h-md-saturn lg:max-h-lg-saturn lg:ml-[6.6875rem]',
+  Uranus: 'max-h-uranus md:max-h-md-uranus lg:max-h-lg-uranus lg:ml-[5.375rem]',
+  Neptune: 'max-h-neptune md:max-h-md-neptune lg:max-h-lg-neptune lg:ml-[5.625rem]',
 }
 
 export default function Content({ planet, tabs }: ContentProps) {
@@ -55,8 +55,8 @@ export default function Content({ planet, tabs }: ContentProps) {
   const currentTab = tabs[activeTab];
  
   return (
-    <main className="grid flex-grow md:grid-cols-2 lg:py-10 lg:gap-y-4 lg:mx-[10.3rem]">
-      <div role="tablist" className="flex flex-row justify-between px-6 border-b border-white/20 md:gap-y-4 md:px-10 md:justify-center md:flex-col md:border-b-0 md:order-last lg:mx-0 lg:px-0 lg:justify-start">
+    <main className="grid flex-grow md:grid-cols-2 lg:py-[4.125rem] lg:gap-y-4 lg:gap-x-[14.1875rem] lg:w-full lg:max-w-[1440px] lg:mx-auto lg:px-[10.3125rem]">
+      <div role="tablist" className="content_tablist flex flex-row justify-between px-6 border-b border-white/20 md:gap-y-4 md:px-10 md:justify-center md:flex-col md:border-b-0 md:order-last lg:mx-0 lg:px-0 lg:w-full lg:max-w-[21.875rem] lg:justify-start lg:place-self-end">
         { tabs.map((tab, index) => (
           <button
             key={tab.title.small}
@@ -76,21 +76,21 @@ export default function Content({ planet, tabs }: ContentProps) {
           </button>    
         ))}
       </div>
-      <div className="grid p-10 md:col-span-2 lg:col-span-1 lg:row-span-2">
-        <Image src={currentTab.image.path} alt={currentTab.image.alt} width={0} height={0} className={`place-self-center ${planetImageSizeClassNames[planet]}`} />
+      <div className="content_planet_image grid h-fit pt-16 pb-16 md:pt-24 md:pb-[4.9375rem] md:col-span-2 lg:col-span-1 lg:row-span-2">
+        <div className={`content_planet_image_wrapper h-full w-auto ml-auto mr-auto lg:mr-0 lg:w-full ${planetImageSizeClassNames[planet]}`}>
+          <img src={currentTab.image.path} alt={currentTab.image.alt} className={`h-full w-auto`} />
+        </div>
         { currentTab.hoverImage && (
-          <Image
+          <img
             src={currentTab.hoverImage.path}
             alt={currentTab.hoverImage.alt}
-            width={170}
-            height={170}
-            className="absolute justify-self-center self-center mt-[8rem] md:mt-[12rem] lg:mt-[20rem] h-[76px] w-auto md:h-[126px] lg:h-[199px]"
+            className="absolute justify-self-center self-center mt-[8rem] md:mt-[12rem] lg:mt-[20rem] h-[76px] md:h-[126px] lg:h-[199px]"
           />)}
       </div>
-      <div className="flex flex-col text-center text-body gap-y-2 mx-6 md:justify-center md:mx-[2.44rem] lg:mx-0 md:text-start md:mr-0 lg:pt-20 lg:justify-between">
-        <h1 className="font-antonio text-heading-2 uppercase lg:text-heading-1">{planet}</h1>
-        <span className="font-league-spartan leading-[1.375rem] lg:leading-body">{currentTab.content}</span>
-        <span className="text-white/50">
+      <div className="content_planet_info flex flex-col text-center text-body mx-6 md:justify-center md:mx-[2.44rem] lg:max-w-[21.875rem] lg:place-self-end md:text-start md:mr-0 lg:pt-[3.75rem] lg:justify-between">
+        <h1 className="content_planet_name font-antonio text-heading-2 uppercase lg:text-heading-1">{planet}</h1>
+        <span className="content_planet_facts font-league-spartan text-heading-4 lg:text-body tracking-[0px] leading-[1.375rem] lg:leading-body mt-[1rem]">{currentTab.content}</span>
+        <span className="content_planet_source text-white/50 mt-[0.625rem] text-heading-3 leading-heading-3">
           Source: <Link target="_blank" href={currentTab.source} className="underline font-spartan-bold">Wikipedia<Image src="/images/icon-source.svg" alt="External link icon" width={12} height={12} className="inline-block ml-1" /></Link>
         </span>
       </div>
